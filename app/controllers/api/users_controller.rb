c953cabba2e783f1e83dbb68c2946b9a573c5a99
mkochalko <matthew.kochalko@gmail.com>
2020-01-07 16:22:21 -0800

    Setting up Heroku

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -7,7 +7,7 @@ class Api::UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        debugger
+        
         if @user 
             login(@user)
             render :show
