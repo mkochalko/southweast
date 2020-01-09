88029daaced104b96203c6be306252da495b8aa5
mkochalko <matthew.kochalko@gmail.com>
2020-01-08 18:57:34 -0800

    fixed merges

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -9,7 +9,7 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Helmet } from 'react-helmet';
 
 const TITLE = "SouthWeast"
-    // < img src = 'http://cdn.shopify.com/s/files/1/0891/8314/products/Patrick_Star_mem_515a0f9e16255_grande.jpeg?v=1459072430' alt = 'southweast' height = '18px' width = '15px' />
+// < img src = 'http://cdn.shopify.com/s/files/1/0891/8314/products/Patrick_Star_mem_515a0f9e16255_grande.jpeg?v=1459072430' alt = 'southweast' height = '18px' width = '15px' />
 const App = () => (
     <div>
         <Helmet>
@@ -19,18 +19,9 @@ const App = () => (
             <NavBarContainer />
         </header>
         <Switch>
-<<<<<<< HEAD
-            {/* <Route path="/signup" component={SignUpContainer}/> */}
-            {/* <Route path="/users/:userId" component={UsersShowContainer}/> */}
-=======
-            <Route exact path='/' component={Home}/>
-            <AuthRoute path="/signup" component={SignUpContainer}/>
-<<<<<<< HEAD
-            <Route path="/users/:userId" component={UsersShowContainer}/>
->>>>>>> dev
-=======
-            <ProtectedRoute path="/users/:userId" component={UsersShowContainer}/>
->>>>>>> dev
+            <Route exact path='/' component={Home} />
+            <AuthRoute path="/signup" component={SignUpContainer} />
+            <ProtectedRoute path="/users/:userId" component={UsersShowContainer} />
         </Switch>
         <Footer />
     </div>
