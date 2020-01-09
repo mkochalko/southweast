cc75ce9e8e6c513b6bbf473f280853b3aaf0405d
mkochalko <matthew.kochalko@gmail.com>
2020-01-09 15:39:42 -0800

    Fixing merges when pushing userAuth issue fixes to master

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -24,10 +24,7 @@ class SessionForm extends React.Component {
 
     handleSubmit(e) {
         e.preventDefault();
-<<<<<<< HEAD
-=======
         this.props.clearErrors()
->>>>>>> dev
         this.props.action(this.state)
     }
 
