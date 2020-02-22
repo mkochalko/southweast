712266b91dbe759fa98011810bf9c3161499fb99
mkochalko <matthew.kochalko@gmail.com>
2020-02-22 15:23:58 -0800

    Setup autocomplete airport search

--- frontend/components/flights/book_flight.jsx
+++ frontend/components/flights/book_flight.jsx
@@ -9,12 +9,63 @@ class BookFlight extends React.Component {
         super(props)
 
         this.state = this.props.trip
+        this.airportCodes = ['BHM', 'DHN', 'HSV', 'MOB', 'MGM', 'ANC', 'FAI', 'JNU', 'FLG', 'PHX', 'TUS', 'YUM', 'FYV', 'LIT', 
+        'XNA', 'BUR', 'FAT', 'LGB', 'LAX', 'OAK', 'ONT', 'PSP', 'SMF', 'SAN', 'SFO', 'SJC', 'SNA', 'ASE', 'COS', 'DEN', 'GJT', 
+        'PUB', 'BDL', 'HVN', 'IAD', 'DCA', 'DAB', 'FLL', 'RSW', 'JAX', 'EYW', 'MIA', 'MCO', 'PNS', 'PIE', 'SRQ', 'TPA', 'PBI', 
+        'PFN', 'ATL', 'AGS', 'SAV', 'ITO', 'HNL', 'OGG', 'KOA', 'LIH', 'BOI', 'MDW', 'ORD', 'MLI', 'PIA', 'EVV', 'FWA', 'IND', 
+        'SBN', 'CID', 'DSM', 'ICT', 'LEX', 'SDF', 'BTR', 'MSY', 'SHV', 'AUG', 'BGR', 'PWM', 'BWI', 'BOS', 'HYA', 'ACK', 'ORH', 
+        'BTL', 'DTW', 'DET', 'FNT', 'GRR', 'AZO', 'LAN', 'MBS', 'DLH', 'MSP', 'RST', 'GPT', 'JAN', 'MCI', 'STL', 'SGF', 'BIL', 
+        'LNK', 'OMA', 'LAS', 'RNO', 'MHT', 'ACY', 'EWR', 'TTN', 'ABQ', 'ALM', 'ALB', 'BUF', 'ISP', 'JFK', 'LGA', 'SWF', 'ROC', 
+        'SYR', 'HPN', 'AVL', 'CLT', 'FAY', 'GSO', 'RDU', 'INT', 'BIS', 'FAR', 'CAK', 'CVG', 'CLE', 'CMH', 'DAY', 'TOL', 'OKC', 
+        'TUL', 'EUG', 'PDX', 'HIO', 'SLE', 'ABE', 'ERI', 'MDT', 'PHL', 'PIT', 'AVP', 'PVD', 'CHS', 'CAE', 'GSP', 'MYR', 'PIR', 
+        'RAP', 'FSD', 'TRI', 'CHA', 'TYS', 'MEM', 'BNA', 'AMA', 'AUS', 'CRP', 'DAL', 'DFW', 'ELP', 'HOU', 'IAH', 'LBB', 'MAF', 
+        'SAT', 'SLC', 'BTV', 'MPV', 'RUT', 'IAD', 'PHF', 'ORF', 'RIC', 'ROA', 'PSC', 'SEA', 'GEG', 'CRW', 'CKB', 'HTS', 'GRB', 
+        'MSN', 'MKE', 'CPR', 'CYS', 'JAC', 'RKS',]
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.onChange = this.onChange.bind(this);
         this.onDateChange = this.onDateChange.bind(this);
         this.roundTrip = this.roundTrip.bind(this);
         this.oneWayTrip = this.oneWayTrip.bind(this);
+        this.airportDropdown = this.airportDropdown.bind(this);
+        this.airportArrivalDropdown = this.airportArrivalDropdown.bind(this);
+        this.filterAirportDropdown = this.filterAirportDropdown.bind(this);
+        this.filterAirportArrivalDropdown = this.filterAirportArrivalDropdown.bind(this);
+    }
+
+    componentDidMount() {
+        let dropdownUl = document.getElementById("airport-dropdown-ul")
+        for (let i = 0; i < this.airportCodes.length; i++) {
+            dropdownUl.innerHTML += `<li key=${i}>${this.airportCodes[i]}</li>`
+        }
+        dropdownUl.addEventListener("click", (e) => {
+            console.log(e.target.innerText)
+            this.setState({o1: e.target.innerText}, () => {
+                console.log(this.state)
+                let input = document.getElementById("departure-flight-input")
+                // console.log(input)
+                input.value = this.state.o1
+            })
+
+            let dropdown = document.getElementsByClassName("airport-dropdown")
+            $(dropdown).css("display", 'none')
+        })
+        let dropdownArrivalUl = document.getElementById("airport-arrival-dropdown-ul")
+        for (let i = 0; i < this.airportCodes.length; i++) {
+            dropdownArrivalUl.innerHTML += `<li key=${i}>${this.airportCodes[i]}</li>`
+        }
+        dropdownArrivalUl.addEventListener("click", (e) => {
+            console.log(e.target.innerText)
+            this.setState({d1: e.target.innerText}, () => {
+                console.log(this.state)
+                let input = document.getElementById("arrival-flight-input")
+                // console.log(input)
+                input.value = this.state.d1
+            })
+
+            let dropdown = document.getElementsByClassName("airport-arrival-dropdown")
+            $(dropdown).css("display", 'none')
+        })
     }
 
     handleSubmit(e) {
@@ -67,6 +118,53 @@ class BookFlight extends React.Component {
         returnDate.className = 'return-date-disabled'
     }
 
+    filterAirportDropdown(text) {
+        let filter = text.toUpperCase();
+        let ul = document.getElementById("airport-dropdown-ul");
+        let li = Array.from(ul.getElementsByTagName('li'));
+        let i;
+
+        for (i = 0; i < li.length; i++) {
+            let txtValue = li[i].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+
+    filterAirportArrivalDropdown(text) {
+        let filter = text.toUpperCase();
+        let ul = document.getElementById("airport-arrival-dropdown-ul");
+        let li = Array.from(ul.getElementsByTagName('li'));
+        let i;
+
+        for (i = 0; i < li.length; i++) {
+            let txtValue = li[i].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+
+    airportDropdown(e) {
+        this.filterAirportDropdown(e.currentTarget.value)
+        let dropdown = document.getElementsByClassName("airport-dropdown")
+        $(dropdown).css("display", 'block')
+
+    }
+
+    airportArrivalDropdown(e) {
+        this.filterAirportArrivalDropdown(e.currentTarget.value)
+        let dropdown = document.getElementsByClassName("airport-arrival-dropdown")
+        $(dropdown).css("display", 'block')
+
+    }
+
+
     render() {
         // console.log(this.props)
         if (this.props.errors.length > 0) {
@@ -94,9 +192,12 @@ class BookFlight extends React.Component {
                                     <div className="flight-search-form-single-input-grouping">
                                         <h4>Depart</h4>
                                         <input type="text"
-                                            onChange={this.onChange("o1")}
+                                            id="departure-flight-input"
+                                            onChange={this.onChange('o1')}
+                                            onInput={this.airportDropdown}
                                             value={this.state.o1}
                                         />
+                                        <div className="airport-dropdown"><ul id="airport-dropdown-ul" className="airport-dropdown-ul"></ul></div>
                                     </div>
 
                                     <div className="flight-search-form-single-input-grouping datepicker">
@@ -124,9 +225,12 @@ class BookFlight extends React.Component {
                                     <div className="flight-search-form-single-input-grouping">
                                         <h4>Arrive</h4>
                                         <input type="text" 
+                                            id="arrival-flight-input"
                                             onChange={this.onChange("d1")}
                                             value={this.state.d1}
+                                            onInput={this.airportArrivalDropdown}
                                         />
+                                        <div className="airport-arrival-dropdown"><ul id="airport-arrival-dropdown-ul" className="airport-arrival-dropdown-ul"></ul></div>
                                     </div>
                                     <div className="flight-search-form-single-input-grouping datepicker">
                                         <h4>Return Date</h4>
